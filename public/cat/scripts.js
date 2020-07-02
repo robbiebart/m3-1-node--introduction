6cb80f58e5a6b2c9eb29bdd49e0d9505e9f51963
Robbie Bart <adm@AdminisatorsAir.lan>
2020-07-02 16:19:20 -0400

    1

--- public/cat/scripts.js
+++ public/cat/scripts.js
@@ -1,1 +1,41 @@
-// here there be JS, yarrr ☠️
+const messageInput = document.querySelector("#user-input");
+const conversationElem = document.querySelector("#conversation-container");
+const handleFocus = () => {
+  messageInput.focus();
+};
+
+const sendMessage = (event) => {
+  // prevent the default "page reload" from occurring.
+  event.preventDefault();
+
+  const updateConversation = (message) => {
+    console.log(message);
+    // deconstruct the message object
+    const { author, text } = message;
+    // create a <p> element
+    const messageElem = document.createElement("p");
+    // add a 'message' class and a class based on the author
+    messageElem.classList.add("message", author);
+    // add the text message to the element
+    messageElem.innerHTML = `<span>${text}</span>`;
+    // append the element to the conversation
+    conversationElem.appendChild(messageElem);
+    conversationElem.scrollTop = conversationElem.scrollHeight;
+    handleFocus();
+    if (author === "user") {
+      messageInput.value = "";
+    }
+  };
+  const message = { author: "user", text: messageInput.value };
+  updateConversation(message);
+
+  // This is a 'GET' call to the /cat-message endpoint.
+  fetch("/cat-message")
+    .then((res) => res.json())
+    .then((data) => {
+      updateConversation(data.message);
+    });
+  handleFocus();
+
+  console.log(messageInput.value);
+};
